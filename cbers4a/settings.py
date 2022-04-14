7305d3540cf5971a7569bc3a92594803d8371baa
HumbertoDiego <humberto-xingu@live.com>
2022-04-14 12:41:27 -0400

    Ocultação de dados

--- cbers4a/settings.py
+++ cbers4a/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-from secrets import USERNAME_POST,PASSWORD_POST
+from secrets import USERNAME_POST,PASSWORD_POST,DJANGO_SECRET_KEY
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-o@^=72$q^i3840)a-1hmyn79xp+@6gx0f25n_bg_1^m082jt#h'
+SECRET_KEY = DJANGO_SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
