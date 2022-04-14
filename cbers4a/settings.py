2f8dcac70158e654c385e4e6d6226bc61820882c
HumbertoDiego <humberto-xingu@live.com>
2022-04-14 12:50:49 -0400

    1st run

--- cbers4a/settings.py
+++ cbers4a/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-from secrets import USERNAME_POST,PASSWORD_POST,DJANGO_SECRET_KEY
+from cbers4a_secrets import USERNAME_POST,PASSWORD_POST,DJANGO_SECRET_KEY
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
