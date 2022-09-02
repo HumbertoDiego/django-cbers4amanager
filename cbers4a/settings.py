ecd46f528b8742af52f11acc4da4ea964ab20dac
HumbertoDiego <humberto-xingu@live.com>
2022-09-02 11:01:42 -0400

    rename table titles

--- cbers4a/settings.py
+++ cbers4a/settings.py
@@ -12,16 +12,14 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
-from cbers4a_secrets import DJANGO_SECRET_KEY
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = DJANGO_SECRET_KEY
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'qqcoisa')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
